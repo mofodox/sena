9611dba9ef69d450c1d1ad0d8a453ce3af5308eb
Khairul Akmal <khairulakmal.dev@gmail.com>
2021-10-18 16:33:45 +0800

    chore: stuff for initialising the application

--- backend/main.go
+++ backend/main.go
@@ -1,11 +1,24 @@
 package main
 
 import (
+	"log"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/joho/godotenv"
+	"github.com/mofodox/sena/database"
 )
 
 func main() {
+	if err := godotenv.Load(".env"); err != nil {
+		log.Printf("unable to load the .env file: %s\n", err.Error())
+	} else {
+		log.Println("Successfully load the .env file")
+	}
+
+	// Initialise the connection to the database
+	database.ConnectDB()
+
 	app := fiber.New()
 
 	// middlewares
